e117ba3b19aa03e5866159ea7695b54df3d4fedb
kjhung8-rgb <kjhung8@gmail.com>
2026-05-18 14:34:02 +0900

    new file:   src/cobot3/cobot_core/.backup_baseaction_refactor_20260518_143014/cobot_core/action_manager.py 	new file:   src/cobot3/cobot_core/.backup_baseaction_refactor_20260518_143014/cobot_core/action_node.py 	new file:   src/cobot3/cobot_core/.backup_baseaction_refactor_20260518_143014/cobot_core/base_action.py 	new file:   src/cobot3/cobot_core/.backup_baseaction_refactor_20260518_143014/cobot_core/command_router.py 	new file:   src/cobot3/cobot_core/.backup_baseaction_refactor_20260518_143014/cobot_core/task/patrol.py 	new file:   src/cobot3/cobot_core/.backup_baseaction_refactor_20260518_143014/launch/robot_system.launch.py 	new file:   src/cobot3/cobot_core/.backup_baseaction_refactor_20260518_143014/test_commands/jetbot_test.json 	modified:   src/cobot3/cobot_core/cobot_core/action_manager.py 	modified:   src/cobot3/cobot_core/cobot_core/action_node.py 	modified:   src/cobot3/cobot_core/cobot_core/base_action.py 	modified:   src/cobot3/cobot_core/cobot_core/command_router.py 	new file:   src/cobot3/cobot_core/cobot_core/robot/__init__.py 	new file:   src/cobot3/cobot_core/cobot_core/robot/jetbot_base_action.py 	new file:   src/cobot3/cobot_core/cobot_core/robot/mobile_base_action.py 	new file:   src/cobot3/cobot_core/cobot_core/robot/spot_base_action.py 	modified:   src/cobot3/cobot_core/cobot_core/task/patrol.py 	modified:   src/cobot3/cobot_core/launch/robot_system.launch.py

---
+++ src/cobot3/cobot_core/cobot_core/robot/__init__.py
@@ -0,0 +1,9 @@
+from .mobile_base_action import MobileBaseAction
+from .jetbot_base_action import JetbotBaseAction
+from .spot_base_action import SpotBaseAction
+
+__all__ = [
+    "MobileBaseAction",
+    "JetbotBaseAction",
+    "SpotBaseAction",
+]
