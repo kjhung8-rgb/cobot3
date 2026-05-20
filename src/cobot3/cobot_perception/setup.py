2612127e6f73c536369074d676d7ca55abd8515c
luckygoaeng <yhy030721@sookmyung.ac.kr>
2026-05-20 20:24:31 +0900

    좌표 마커 mock파일 제거

--- src/cobot3/cobot_perception/setup.py
+++ src/cobot3/cobot_perception/setup.py
@@ -26,7 +26,6 @@ setup(
     entry_points={
         'console_scripts': [
             'survivor_detector = cobot_perception.survivor_detector:main',
-            'survivor_pose_mock = cobot_perception.survivor_pose_mock:main',
             'survivor_pose_to_marker = cobot_perception.survivor_pose_to_marker:main',
             'rotate_on_arrival = cobot_perception.rotate_on_arrival:main',
             'camera_coverage_tracker = cobot_perception.camera_coverage_tracker:main',
